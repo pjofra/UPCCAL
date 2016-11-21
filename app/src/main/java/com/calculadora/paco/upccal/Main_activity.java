56296ea167a76372e9a7a9d949c452ec5f68f70d
pjofra <paco@jofra.cat>
2016-11-21 16:24:11 +0100

    UPCCAL
    UPCCAL

---
+++ app/src/main/java/com/calculadora/paco/upccal/Main_activity.java
@@ -0,0 +1,13 @@
+package com.calculadora.paco.upccal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activty_main);
+    }
+}
